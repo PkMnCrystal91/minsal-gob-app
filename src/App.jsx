6f92af7eb5e8e903ad97317c3acad625c533d8fe
Herbert Gutierrez <dev.gutti1391@gmail.com>
2024-03-15 18:28:40 -0600

    Login page done, Inventory Started

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Login } from "./pages/Login/Login";
+import { Login, Inventory } from "./pages/index";
 import "./App.css";
 
 export const App = () => {
   return (
-    <div className="container-fluid main-app d-flex justify-content-center align-items-center">
-      <Login />
-    </div>
+    <>
+      <Inventory />
+    </>
   );
 };
---
+++ src/pages/Inventory/Inventory.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../../components/index";
+import "./Inventory.css";
+
+export const Inventory = () => {
+  return (
+    <div className="container-fluid bg-secondary main-inventory">
+      <Navbar />
+    </div>
+  );
+};
